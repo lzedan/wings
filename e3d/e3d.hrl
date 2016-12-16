eb57d61035bbfb95eebd5136b31d2e23036ec085
Dan Gudmundsson <dgud@users.sourceforge.net>
2016-12-16 15:54:44 +0100

    e3d: Add bvh, add more functionality to bv and kd3 incompatible
    Removed qbvh and replaced it with bvh with the same functionality.
    Also added [tri_]intersect for fast intersection testing to bvh.
    
    Added some ray casting functionality mainly for testing bvh.
    
    Changed order {Pos,Key} => {Key,Pos} in kd3 since most often the postion
    will not be the key, i.e. to avoid re-ordering the tuples.
    
    Add line_line_intersect and supporting functions to e3d.
    I need this and it can be useful elsewhere, also
    largest_dir and project_2d can be used in e3d_bvh.

--- e3d/e3d.hrl
+++ e3d/e3d.hrl
@@ -24,7 +24,15 @@
 
 -type e3d_transform() :: #e3d_transf{}.
 
-  
+
+-record(ray,
+	{o::e3d_vec:point(),
+         d::e3d_vec:vector(),
+	 n::float(),                            % Near, far (or MinT MaxT)
+         f::float()}).
+
+-type e3d_ray() :: #ray{}.
+
 -record(e3d_face,
 	{vs=[],				        %List of vertex indices.
 	 vc=[],					%Vertex color indices.
@@ -59,5 +67,3 @@
 	 creator="", 				%Creator string.
 	 dir					%Directory for file.
 	}).
-
-
